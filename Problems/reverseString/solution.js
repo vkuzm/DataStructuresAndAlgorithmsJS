22d65eeb02d28bfc0e9dfec11a9758f88dbf3382
Vit <test@epam.com>
2020-07-07 18:14:10 +0300

    Add reverse string"

--- Problems/reverseString/solution.js
+++ Problems/reverseString/solution.js
@@ -5,6 +5,8 @@
 console.log(reverse('Hello world'));
 console.log(reverse2('Hello world'));
 console.log(reverse3('Hello world'));
+console.log(reverse4('Hello world'));
+console.log(reverseRecursion('Hello world'));
 
 // Time Complexity = O(n)
 // Space Complexity = O(1)
@@ -36,4 +38,32 @@ function reverse2(string) {
 // Space Complexity = O(1)
 function reverse3(string) {
   return string.split('').reverse().join('');
+}
+
+// Time Complexity = O(n)
+// Space Complexity = O(n)
+function reverse4(string) {
+  const array = string.split('');
+  let counter = 0;
+
+  for (let i = 0; i < array.length / 2; i++) {
+    const temp = array[counter];
+    const firstIndex = counter;
+    const lastIndex = array.length - 1 - counter;
+
+    array[firstIndex] = array[lastIndex];
+    array[lastIndex] = temp;
+    counter++;
+  }
+  return array.join('');
+}
+
+// Time Complexity = O(n)
+// Space Complexity = O(1)
+function reverseRecursion(str) {
+  if (str === '') {
+    return '';
+  } else {
+    return reverseRecursion(str.substr(1)) + str.charAt(0);
+  }
 }
