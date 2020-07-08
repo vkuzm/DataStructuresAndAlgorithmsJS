5ce2403a36b0e3c12d23e0d202cbd7b3e4b95a3d
Vit <test@epam.com>
2020-07-08 17:10:41 +0300

    add quick sort

--- Sorting/MergeSort.js
+++ Sorting/MergeSort.js
@@ -8,24 +8,20 @@ function mergeSort(array) {
   const left = array.slice(0, middle);
   const right = array.slice(middle);
 
-  return merge(
-    mergeSort(left),
-    mergeSort(right)
-  );
+  return merge(mergeSort(left), mergeSort(right));
 }
 
-function merge(left, right){
+function merge(left, right) {
   const result = [];
   let leftIndex = 0;
   let rightIndex = 0;
-  while(leftIndex < left.length &&
-  rightIndex < right.length){
-    if(left[leftIndex] < right[rightIndex]){
+  while (leftIndex < left.length && rightIndex < right.length) {
+    if (left[leftIndex] < right[rightIndex]) {
       result.push(left[leftIndex]);
       leftIndex++;
-    } else{
+    } else {
       result.push(right[rightIndex]);
-      rightIndex++
+      rightIndex++;
     }
   }
   return result.concat(left.slice(leftIndex)).concat(right.slice(rightIndex));
